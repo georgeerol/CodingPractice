0111487871c9e1e87214b3b42f3341070f1cb792
George Fouche <fouliex@gmail.com>
2019-12-07 10:08:40 -0800

    Add couple of array problems

--- src/main/java/datastructures/array/CheckRemoveEven.java
+++ src/main/java/datastructures/array/CheckRemoveEven.java
@@ -1,7 +1,48 @@
 package datastructures.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by George Fouche on 12/6/19.
  */
 public class CheckRemoveEven {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 10, 6, 3};
+        System.out.println(Arrays.toString(removeEvenExp1(arr)));
+        System.out.println(Arrays.toString(removeEvenExp2(arr)));
+
+    }
+
+    private static int[] removeEvenExp2(int[] arr) {
+        int oddElements = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) oddElements++;
+        }
+
+        int[] result = new int[oddElements];
+        int resultIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 !=0)result[resultIndex++] = arr[i];
+        }
+
+        return result;
+    }
+
+    private static int[] removeEvenExp1(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                result.add(arr[i]);
+            }
+        }
+        int[] newArr = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            newArr[i] = result.get(i);
+        }
+        return newArr;
+    }
 }
+
---
+++ src/main/java/datastructures/array/CheckSum.java
@@ -0,0 +1,32 @@
+package datastructures.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by George Fouche on 12/6/19.
+ */
+public class CheckSum {
+
+    public static void main(String[] args) {
+        int[]arr = {1,21,3,14,5,60,7,6};
+        int value = 27;
+
+        System.out.println(Arrays.toString(findSumExp1(arr,value)));
+    }
+
+    private static int[] findSumExp1(int[] arr, int value) {
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for(int i =0; i <arr.length;i++){
+            int temp = value -arr[i];
+            if(map.containsKey(temp)){
+                return new int[]{arr[map.get(temp)],arr[i]};
+            }else{
+                map.put(arr[i],i);
+            }
+        }
+        return new int[0];
+    }
+
+}
