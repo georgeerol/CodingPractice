8a362194e7184d1547fc22974761d6904ab180e4
George Fouche <fouliex@gmail.com>
2019-12-10 22:42:07 -0800

    Add Stack and Queu

---
+++ src/main/java/educative/datastructures/stackandqueues/FinBinChallenge.java
@@ -0,0 +1,11 @@
+package educative.datastructures.stackandqueues;
+
+/**
+ * Created by George Fouche on 12/10/19.
+ */
+public class FinBinChallenge {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/main/java/educative/datastructures/stackandqueues/Stack.java
@@ -0,0 +1,65 @@
+package educative.datastructures.stackandqueues;
+
+/**
+ * Created by George Fouche on 12/10/19.
+ */
+public class Stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+
+    private void push(int data) {
+        if (isFull()) throw new RuntimeException("Stack is full");
+        stackArray[++top] = data;
+    }
+
+    private int pop() {
+        if (isEmpty()) throw new RuntimeException("Stack is empty");
+        return stackArray[top--];
+    }
+
+    private int peek() {
+        if (isEmpty()) throw new RuntimeException("Stack is empty");
+        return stackArray[top];
+
+    }
+
+
+    private boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    private boolean isEmpty() {
+        return top < 0;
+    }
+
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(20);
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+
+    }
+
+    String view() {
+        String result = "[";
+        for (int data : stackArray) {
+            result += data + " ";
+        }
+        result += "]";
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return view();
+    }
+}
