760335c5933e12d6c07f83eb461737f9c5e3cd86
George Fouche <fouliex@gmail.com>
2020-01-03 23:41:42 -0800

    Update Check Rotate Array problem

--- src/main/java/fundamental/datastructures/array/CheckRotateArray.java
+++ src/main/java/fundamental/datastructures/array/CheckRotateArray.java
@@ -8,12 +8,27 @@ import java.util.Arrays;
 public class CheckRotateArray {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr1= {1, 2, 3, 4, 5};
+        int[] arr2= {1, 2, 3, 4, 5};
 
-        System.out.println(Arrays.toString(rotateArrayExp1(arr)));
+        System.out.println(Arrays.toString(rotateArrayExp1(arr1,1)));
+        System.out.println(Arrays.toString(rotateArrayExp2(arr2)));
     }
 
-    private static int[] rotateArrayExp1(int[] nums) {
+    private static int[] rotateArrayExp1(int[] arr,int k) {
+        int[] tempArray = new int[arr.length];
+        for(int i =0; i< arr.length;i++){
+            tempArray[(i+k) % arr.length] = arr[i];
+        }
+        for(int i = 0; i<arr.length;i++){
+            arr[i] = tempArray[i];
+        }
+
+        return arr;
+
+    }
+
+    private static int[] rotateArrayExp2(int[] nums) {
         int k = 1;
         int temp, previous;
         for (int i = 0; i < k; i++) {
